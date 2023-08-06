5fea8b77a8b2879accaa90e42392b86101970009
Mohamed ZAARAOUI <115699524+ZAARAOUI999@users.noreply.github.com>
2023-08-06 12:32:13 +0100

    Create _utils.py

---
+++ hypermat/_models/_utils.py
@@ -0,0 +1,31 @@
+"""
+    HyperMAT
+    Created August 2023
+    Copyright (C) Mohamed ZAARAOUI
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+"""
+
+def isochoric(func, grad, **kwargs):
+    """Isochoric part of strain energy"""
+    w_iso = func(grad, **kwargs)
+    return w_iso
+
+def volumetric(grad, **kwargs):
+    """volumetric part of strain energy"""
+    det = grad.invariants[-1]
+    bulk = kwargs.pop('K')
+    w_vol = 0.5 * bulk * (det - 1.0)**2.0
+    return w_vol
