314f804e8e58cd6a8ce3f2daab72d7f8b9b66b38
Mohamed ZAARAOUI <115699524+ZAARAOUI999@users.noreply.github.com>
2023-12-19 17:50:26 +0100

    Update __init__.py

--- hypermat/_calibration/__init__.py
+++ hypermat/_calibration/__init__.py
@@ -1,2 +1,7 @@
+
+####################### - بــسم الله الرحمــان الرحيــم - #####################
+
+"""Calibration Module"""
+
 from ._load import Test, Uniaxial, EquiBiaxial, PureShear
 from ._utils import read_file, to_dict
