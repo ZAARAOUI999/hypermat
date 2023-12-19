da89ee3f1aef45d53e1c2f2dbb72d9d8cf3cd5a9
Mohamed ZAARAOUI <115699524+ZAARAOUI999@users.noreply.github.com>
2023-12-19 17:53:17 +0100

    Update __init__.py

--- hypermat/_models/__init__.py
+++ hypermat/_models/__init__.py
@@ -1,2 +1,6 @@
-from ._strain_energy import StrainEnergy
+####################### - بــسم الله الرحمــان الرحيــم - #####################
+
+"""Material Formulations Module"""
+
+from ._energy import StrainEnergy
 from ._material import *
